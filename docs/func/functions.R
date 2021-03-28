7f01073979f45b24f8e1bb4ff17ee4b81c09b1e9
Franzi Löw <franzi@localyzeapp.com>
2021-03-28 14:12:29 +0200

    more on stm results

--- docs/func/functions.R
+++ docs/func/functions.R
@@ -5,15 +5,48 @@
 ###########################################
 plot_topic_distr_document <- function(doc_id){
   title <- theta %>% filter(doc_index == doc_id) %>% select(title, source)
-  theta %>% 
-    filter(doc_index == doc_id) %>% 
+  temp_df <- theta %>% filter(doc_index == doc_id) 
+  
+  temp_df %>% 
     ggplot(aes(reorder(topic,topic), theta)) +
     geom_col() +
-    #coord_flip() +
+    geom_text(aes(label=ifelse(theta > 0.02, round(theta,2), '')),
+              size = 2, color = 'red', vjust = 0
+    ) +
     ylim(c(0,1)) +
-    labs(x = NULL, y = "Topic proportion",
-         caption = paste0(title$title," (",title$source,")")) +
-    theme(axis.title.y = element_text(angle = 90))
+    labs(x = "Topic", y = "Topic proportion",
+         title = paste0(title$title," (",title$source,")")) +
+    theme(
+      axis.title = element_text(size=4),
+      axis.title.y = element_text(angle = 90))
+}
+
+#######################################
+## Plot Topic distribution over time ##
+#######################################
+plot_topic_distr_time <- function(topic_filter, source_filter_array) {
+  topic_label <-theta[theta$topic == topic_filter, ]$topic_label[1]
+  
+  theta %>% 
+    filter(topic == topic_filter) %>% 
+    filter(source %in% source_filter_array) %>% 
+    group_by(source, year_week, type) %>% 
+    summarise(theta_mean = mean(theta, na.rm = T)) %>% 
+    ggplot(aes(year_week, theta_mean,
+               group = source, color = source)) + 
+    ylim(c(0,0.35)) +
+    geom_line(size = 0.3) +
+    geom_vline(xintercept = election_date, linetype = 'dashed', size = 0.3) +
+    scale_colour_hc()+
+    scale_x_date(date_breaks = "1 month", labels = date_format("%m-%Y")) +
+    labs(x = NULL, y = 'Topic proportion', 
+         title = 'Mean topic proportion',
+         subtitle = topic_label) +
+    theme(
+      axis.title = element_text(size=4),
+      axis.title.y = element_text(angle = 90),
+      legend.title = element_blank()
+    )
 }
 
 # 2. Cosine similarity ----
@@ -30,7 +63,7 @@ calc_cos_sim <- function(doc_code, gamma_mat = gamma) {
   doc_col_index <- which(colnames(gamma_mat) == as.character(doc_code))
   # get the date of the doc
   date_target <- filter(model_df, doc_index == doc_code)$date
-  # get all doc_codes +/- 4 days
+  # get all doc_codes within -7 days
   date_seq <- seq(from=date_target-6,by="day",length.out = 7)
   doc_code_pairs <- filter(model_df, date %in% date_seq)$doc_index
   # calculate cosine similarity for each document based on gamma
@@ -73,13 +106,9 @@ df_prep_ols <- function(cosine_df) {
               by = "doc_code2"
     ) %>%
     mutate(
-      election1 = ifelse(date1 <= election_date, "pre", "post"),
-      election2 = ifelse(date2 <= election_date, "pre", "post"),
       election_dummy = as.factor(ifelse(date1 <= election_date, "pre", "post"))
       ) %>% 
-    filter(election1 == election2) %>% 
     filter(type2 == "press") %>% 
-    select(-election1, election2) %>% 
     group_by(date1, source1, source2, election_dummy) %>% 
     summarise(cos_sim = mean(cos_sim, na.rm = T)) %>% 
     ungroup()
@@ -93,11 +122,13 @@ plot_cosine_sim_ols <- function(df, target) {
          aes(date1, log(cos_sim))) +
     geom_point(size = 0.2, alpha = 0.5) +
     geom_smooth(method = lm, 
+                color='red',
                 formula = y ~ x,
                 se=FALSE, size = 0.3) +
+    scale_y_continuous(limits = c(-5,-1)) +
     scale_x_date(date_breaks = "2 month", labels = date_format("%m-%Y")) +
     facet_wrap(~source2, nrow = 1) +
-    labs(x=NULL, y = "ln(cosine similarity)",
+    labs(x=NULL, y = NULL,
          caption = paste0(target)) +
     theme(
       strip.background = element_blank(),
@@ -115,11 +146,12 @@ plot_cosine_sim_rd <- function(df, target) {
     geom_vline(xintercept = as.Date('2017-09-24'), size = 0.3, linetype = 'dashed') +
     geom_point(size = 0.2, alpha = 0.5) +
     geom_smooth(method = lm, 
+                color='red',
                 formula = y ~ x,
                 se=FALSE, size = 0.3) +
     scale_x_date(date_breaks = "2 month", labels = date_format("%m-%Y")) +
     facet_wrap(~source2, nrow = 1) +
-    labs(x=NULL, y = "ln(cosine similarity)",
+    labs(x=NULL, y = NULL,
          caption = paste0(target)) +
     theme(
       strip.background = element_blank(),
